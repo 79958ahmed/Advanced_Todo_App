30b052f56ba040a498c6b24f4936d6300876f5f2
ahmed yassin <83839308+79958ahmed@users.noreply.github.com>
2023-12-03 16:15:41 +0200

    New Project

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:advanced_todo_app/core/utils/app_assets.dart';
 import 'package:advanced_todo_app/core/utils/app_colors.dart';
 import 'package:advanced_todo_app/core/utils/app_strings.dart';
+import 'package:advanced_todo_app/features/presentation/auth/screens/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,13 +9,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Advanced Todo App',
 debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: AppColors.Background,
-        body: Center(child: Image.asset(AppAssets.logo),),
-      ),
+      home: SplashScreen(),
     );
   }
 }
